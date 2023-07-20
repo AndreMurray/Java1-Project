a847ed98648612260ad136b32bad6fbd0195f10e
Andre Murray <slushymatis@gmail.com>
2023-07-20 18:48:56 -0500

    current value

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 5;
+        while(i <= 15) {
+            System.out.println("Current value of i: " + i);
+            i++; // Increment i by one
+        }
+
     }
+
 }
